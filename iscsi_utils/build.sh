98eccc56d4d23a5a50b012026833db698c03d729
Chris <chris.chien@suse.com>
2024-03-13 11:58:59 +0800

    Add more packages into iscsi image
    Signed-off-by: Chris <chris.chien@suse.com>

--- iscsi_utils/build.sh
+++ iscsi_utils/build.sh
@@ -4,7 +4,6 @@ LATEST_VERSION=$(curl -w '%{url_effective}' -I -L -s -S ${GITHUB_URL}/latest -o 
 VERSION_TO_BUILD="${1:-${LATEST_VERSION}}"
 ALPINE_VERSION=$(curl  -L -s -S https://raw.githubusercontent.com/k3s-io/k3s/${VERSION_TO_BUILD}/package/Dockerfile | grep 'FROM alpine:' | cut -d ' ' -f 2)
 K3S_VERSION=$(echo ${VERSION_TO_BUILD} | sed -e 's|\+|-|')
-IMAGE_TAG="ghcr.io/fastlorenzo/k3s:${K3S_VERSION}"
+IMAGE_TAG="k3discsi:${K3S_VERSION}"
 echo "Building k3s ${VERSION_TO_BUILD} and publishing ${IMAGE_TAG} (from rancher/k3s:${VERSION_TO_BUILD})"
 cat Dockerfile.template | sed "s/ALPINE_VERSION/${ALPINE_VERSION}/" | sed "s/K3S_VERSION/${K3S_VERSION}/" | docker build -t ${IMAGE_TAG} -
-docker push ${IMAGE_TAG}
